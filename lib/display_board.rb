9003d8be19579f652e47c3a398f61b267d2eeae7
modern-helper-8600 <bennett.melissa1@gmail.com>
2020-01-30 23:57:33 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,16 @@
-# Define display_board that accepts a board and prints
-# out the current state.
+def display_board (game)
+   puts " #{game[0]} | #{game[1]} | #{game[2]} "
+   puts "-----------"
+   puts " #{game[3]} | #{game[4]} | #{game[5]} "
+   puts "-----------"
+   puts " #{game[6]} | #{game[7]} | #{game[8]} "
+end
+
+board =['   ','   ','   ','   ','   ','   ','   ','   ','   ']
+display_board(board)
+
+board =['   ','   ','   ',' X ','   ','   ','   ','   ','   ']
+display_board(board)
+
+board =[' O ','   ','   ','   ','   ','   ','   ','   ','   ']
+display_board(board)
